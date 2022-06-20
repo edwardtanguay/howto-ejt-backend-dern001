65f94cebc7d0e8642094245b0917156795f7bcdb
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-20 23:47:41 +0200

    initial commit

---
+++ src/dataSources/employees_csv.js
@@ -0,0 +1,11 @@
+import * as qfil from '../qtools/qfil.js';
+
+const getEmployees = async () => {
+	const commaRecords = await qfil.getRecordsFromCsvFile('src/data/employees_commas.csv');
+	const semicolonRecords = await qfil.getRecordsFromCsvFile('src/data/employees_semicolons.csv', ';');
+	const records = commaRecords.concat(semicolonRecords);
+	console.log(records);
+	return records;
+}
+
+export default await getEmployees();
